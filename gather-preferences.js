666acb87d900e9e3c044b677fb593c857ad56154
Tobias Sirianni Melo <sirianni.gf@gmail.com>
2023-07-12 18:41:28 -0300

    chore: colorize prompts

--- gather-preferences.js
+++ gather-preferences.js
@@ -17,32 +17,34 @@ exports.gatherPasswordPreferences = async () => {
     useSpecialCharacters: null,
   };
 
+  const color = "\u001b[36m";
+
   preferences.length = await askQuestion(
     rl,
-    "What is the desired password length? ",
+    `${color} What is the desired password length? `,
     true
   );
 
   preferences.useLetters = await askQuestion(
     rl,
-    "Would you like to include letters in your password? [y/n] "
+    `${color} Would you like to include letters in your password? [y/n] `
   );
 
   if (preferences.useLetters) {
     preferences.useUppercase = await askQuestion(
       rl,
-      "Would you like to include uppercase letters in your password? [y/n] "
+      `${color} Would you like to include uppercase letters in your password? [y/n] `
     );
   }
 
   preferences.useNumbres = await askQuestion(
     rl,
-    "Would you like to include numbers in your password? [y/n] "
+    `${color} Would you like to include numbers in your password? [y/n] `
   );
 
   preferences.useSpecialCharacters = await askQuestion(
     rl,
-    "Would you like to include special characters in your password? [y/n] "
+    `${color} Would you like to include special characters in your password? [y/n] `
   );
 
   rl.close();
