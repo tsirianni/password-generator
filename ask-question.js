de16be0f5371da0f9a6710733239ec9850f4a420
Tobias Sirianni Melo <sirianni.gf@gmail.com>
2023-07-12 17:59:12 -0300

    chore: add function to ask for user input

---
+++ ask-question.js
@@ -0,0 +1,30 @@
+"use strict";
+
+exports.askQuestion = function (rlInterface, question, isPassLength) {
+  return new Promise((resolve) => {
+    const acceptableInputs = ["y", "n"];
+    const affirmativeInputs = acceptableInputs.slice(0, 1);
+
+    rlInterface.question(question, (answer) => {
+      // Validate password length
+      if (isPassLength) {
+        if (isNaN(Number(answer))) {
+          console.error(
+            "Invalid input received. The password length must be a number"
+          );
+        }
+      } else {
+        if (!acceptableInputs.includes(answer)) {
+          console.log(
+            "Invalid input received. Please use one of the following: y/n"
+          );
+          process.exit(1);
+        }
+      }
+
+      resolve(
+        isPassLength ? Number(answer) : !!affirmativeInputs.includes(answer)
+      );
+    });
+  });
+};
