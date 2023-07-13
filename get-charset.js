427dff9a259914feb72a268a499507a6492307e1
Tobias Sirianni Melo <sirianni.gf@gmail.com>
2023-07-13 15:07:21 -0300

    docs: documment getCharset function

--- get-charset.js
+++ get-charset.js
@@ -1,5 +1,16 @@
 "use strict";
 
+/**
+
+Generates a character set based on the provided configuration options. It has a base charset containing all of the optinos below, and uses regex to exclude the unwanted elements (numbers, letters, etc.).
+
+@param {Object} options - The configuration options for generating the character set.
+@param {boolean} [options.useLetters=true] - Specifies whether to include letters in the character set.
+@param {boolean} [options.useUppercase=true] - Specifies whether to include uppercase letters in the character set.
+@param {boolean} [options.useNumbers=true] - Specifies whether to include numbers in the character set.
+@param {boolean} [options.useSpecials=true] - Specifies whether to include special characters in the character set.
+@returns {string} The generated character set.
+*/
 const getCharset = ({
   useLetters = true,
   useUppercase = true,
